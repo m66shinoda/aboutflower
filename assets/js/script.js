a1c19cb7c4f9c5422f8dbf471506d6ce381763ed
shinoda <m66.shinoda@gmail.com>
2025-03-17 12:52:50 +0900

    メインビジュアルのスライドショー導入

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,22 @@ $(function () {
     $('#header-btn').on('click', () => {
         $('body').toggleClass('is-openMenu');
     })
-})
+});
+
+
+// ()で囲む → 即時関数　評価して優先させる？
+(function () {
+    'use strict';
+
+    new Swiper('#kv', {
+        loop: true,
+        effect: 'fade',
+        autoplay: {
+            delay: 4000,
+            pauseOnMouseEnter: true,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        }
+    });
+})();
